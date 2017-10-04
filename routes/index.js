d3f939837b798597831cd73684dac085c2c914a3
favl <favl.dk@gmail.com>
2017-10-04 13:34:05 +0200

    updated jenkinsfile

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Loan Broker Component' });
+  res.render('index', { title: 'Loan Broker Component THIS GOT MERGED INTO MASTER' });
 });
 
 module.exports = router;
