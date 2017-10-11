797fb86eeabad6beb2bddc1bf5ef51b589f8737b
Martin Macej <martin@MacBook-Pro-uzivatela-Martin.local>
2017-10-11 12:50:35 +0200

    Server init

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Loan Broker Component THIS GOT MERGED INTO MASTER' });
-});
-
-module.exports = router;
