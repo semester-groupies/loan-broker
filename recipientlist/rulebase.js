adb1c7c5f12b6b881c7facbab93fb010375378a9
favl <favl.dk@gmail.com>
2017-10-21 11:26:40 +0200

    changes

---
+++ recipientlist/rulebase.js
@@ -0,0 +1,37 @@
+var soap = require('soap');
+var express = require('express');
+var app = express();
+var fs = require('fs'),
+    path = require('path'),
+    xmlReader = require('read-xml');
+
+
+var service = {
+  Banks_Service: {
+    Banks_Port: {
+      getBanks: function (args) {
+        console.log(Date().getFullYear())
+        var year = new Date().getFullYear();
+        var n = args
+        console.log('blabla');
+        var allBanks = ['cphbusiness.bankJSON',
+          'cphbusiness.bankXML',
+          '',
+          ''];
+        var ans = [];
+
+        ans = allBanks;
+
+        return ans;
+      }
+    }
+  }
+};
+
+var xml = fs.readFileSync(__dirname + '/rulebase.wsdl', 'utf8');
+var server = app.listen(3031, function () {
+  var host = "127.0.0.1";
+  var port = server.address().port;
+});
+
+soap.listen(server, '/getbanks', service, xml);
