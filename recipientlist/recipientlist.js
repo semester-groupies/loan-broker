b2daf37baa2981ed9a635a3ec5070fdc08b540aa
favl <favl.dk@gmail.com>
2017-10-21 13:22:29 +0200

    json line

---
+++ recipientlist/recipientlist.js
@@ -0,0 +1,3 @@
+var amqp = require('amqplib/callback_api');
+var url = 'amqp://student:cph@datdb.cphbusiness.dk:5672';
+
---
+++ translator/translator-json.js
@@ -0,0 +1,29 @@
+var amqp = require('amqplib/callback_api');
+var url = 'amqp://student:cph@datdb.cphbusiness.dk:5672';
+
+amqp.connect(url, function (err, conn) {
+  conn.createChannel(function (err, chnl) {
+    var q = 'Group11_translator_json';
+    chnl.assertQueue(q,  { durable: false });
+    chnl.consume(q,  function (msg) {
+      requestBank((JSON.parse(msg)));
+    }, {
+      noAck: true
+    });
+  });
+});
+
+function requestBank(message) {
+  message.ssn = message.ssn.replace('-', '');
+  amqp.connect(url, function (err, conn) {
+    conn.createChannel(function (err, chnl) {
+      var exch = 'cphbusiness.bankJSON';
+      chnl.assertExchange(exch, 'fanout', {
+        durable: false
+      });
+      chnl.publish(exch, '', Buffer.from(JSON.stringify(message)), {
+        replyTo: 'Group11_queue_json'
+      });
+    });
+  });
+}
